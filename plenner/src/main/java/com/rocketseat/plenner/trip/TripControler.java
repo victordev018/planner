e81bc1260842ad1fd5240dc939f4d83f03e4701a
João VIctor <joawrt8520@gmail.com>
2024-07-13 22:52:56 -0300

    feat: created uptate endpoint

--- plenner/src/main/java/com/rocketseat/plenner/trip/TripControler.java
+++ plenner/src/main/java/com/rocketseat/plenner/trip/TripControler.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -34,5 +36,26 @@ public class TripControler {
         Optional<Trip> trip = repository.findById(id);
 
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updatedTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload){
+        Optional<Trip> trip = repository.findById(id);
+
+        // verificando se a trip existe
+        if (trip.isPresent()){
+            Trip rawTrip = trip.get();
+            // atualizando iformações da viagem
+            rawTrip.setId(trip.get().getId());
+            rawTrip.setStart_at(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setEnds_at(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setDestination(payload.destination());
+
+            this.repository.save(rawTrip);
+
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
