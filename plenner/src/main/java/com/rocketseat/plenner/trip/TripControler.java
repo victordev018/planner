a92af42a39715f546d64f35e96f3616d6c23a6c8
João VIctor <joawrt8520@gmail.com>
2024-07-13 23:10:58 -0300

    feat: created confirm trip endpoint

--- plenner/src/main/java/com/rocketseat/plenner/trip/TripControler.java
+++ plenner/src/main/java/com/rocketseat/plenner/trip/TripControler.java
@@ -46,13 +46,31 @@ public class TripControler {
         if (trip.isPresent()){
             Trip rawTrip = trip.get();
             // atualizando iformações da viagem
-            rawTrip.setId(trip.get().getId());
             rawTrip.setStart_at(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
             rawTrip.setEnds_at(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
             rawTrip.setDestination(payload.destination());
 
             this.repository.save(rawTrip);
 
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id){
+        Optional<Trip> trip = repository.findById(id);
+
+        // verificando se a trip existe
+        if (trip.isPresent()){
+            Trip rawTrip = trip.get();
+            // atualizando status de viagem
+            rawTrip.setIsConfirmed(true);
+
+            this.repository.save(rawTrip);
+            // chamada do serviço que dispara os emails para os participantes
+            this.participantService.triggerConfirmationEmailToParticipants(id);
             return ResponseEntity.ok(rawTrip);
         }
 
