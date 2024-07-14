7c2c75bd3f01494e20d23cfb2c232f8e1893fb43
João VIctor <joawrt8520@gmail.com>
2024-07-13 23:35:17 -0300

    feat: created participant enitity and table

---
+++ plenner/src/main/java/com/rocketseat/plenner/participant/Participant.java
@@ -0,0 +1,37 @@
+package com.rocketseat.plenner.participant;
+
+import com.rocketseat.plenner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToMany
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
